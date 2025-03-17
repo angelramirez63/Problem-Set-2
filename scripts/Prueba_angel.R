451b62d8d123484f70e0ed2a656a29c33612b5e1
angelramirez63 <ramirezangel6315@gmail.com>
2025-03-16 23:26:02 -0500

    Update Prueba_angel.R

--- scripts/Prueba_angel.R
+++ scripts/Prueba_angel.R
@@ -5,3 +5,26 @@ p_load(tidyverse, rvest, rebus, htmltools, rio, skimr,
        visdat, margins, stargazer, here, VIM, caret, dplyr, stats)
 
 rm(list = ls())
+
+# Crear el directorio 
+wd <- here()
+setwd()
+rm(wd)
+
+# Cargar los datos
+ss <- read.csv("sample_submission.csv") %>% 
+  as_tibble()
+
+tsh <- read.csv("test_hogares.csv") %>% 
+  as_tibble()
+
+trh <- read.csv("train_hogares.csv") %>% 
+  as_tibble()
+
+tsp <- read.csv("test_personas.csv") %>% 
+  as_tibble()
+
+trp <- readRDS("train_personas.rds") %>% 
+  as_tibble() #Por el paso del archivo se convirtió a rds
+
+
